6beabec1f6c62699e8ee727a951b230c59489a1a
Marcy_ <142843705+MarciaCondarco@users.noreply.github.com>
2024-12-03 11:59:43 -0300

    Add files via upload

---
+++ Formulario.Aula.3/Formulario.Aula.3/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Formulario.Aula._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelemail = new System.Windows.Forms.Label();
+            this.labelidade = new System.Windows.Forms.Label();
+            this.labelsobrenome = new System.Windows.Forms.Label();
+            this.labelnome = new System.Windows.Forms.Label();
+            this.buttonFechar = new System.Windows.Forms.Button();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxIdade = new System.Windows.Forms.TextBox();
+            this.textBoxSobrenome = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelemail
+            // 
+            this.labelemail.AutoSize = true;
+            this.labelemail.Location = new System.Drawing.Point(490, 395);
+            this.labelemail.Name = "labelemail";
+            this.labelemail.Size = new System.Drawing.Size(0, 13);
+            this.labelemail.TabIndex = 29;
+            this.labelemail.Click += new System.EventHandler(this.labelemail_Click);
+            // 
+            // labelidade
+            // 
+            this.labelidade.AutoSize = true;
+            this.labelidade.Location = new System.Drawing.Point(489, 355);
+            this.labelidade.Name = "labelidade";
+            this.labelidade.Size = new System.Drawing.Size(0, 13);
+            this.labelidade.TabIndex = 28;
+            this.labelidade.Click += new System.EventHandler(this.labelidade_Click);
+            // 
+            // labelsobrenome
+            // 
+            this.labelsobrenome.AutoSize = true;
+            this.labelsobrenome.Location = new System.Drawing.Point(486, 320);
+            this.labelsobrenome.Name = "labelsobrenome";
+            this.labelsobrenome.Size = new System.Drawing.Size(0, 13);
+            this.labelsobrenome.TabIndex = 27;
+            this.labelsobrenome.Click += new System.EventHandler(this.labelsobrenome_Click);
+            // 
+            // labelnome
+            // 
+            this.labelnome.AutoSize = true;
+            this.labelnome.Location = new System.Drawing.Point(485, 282);
+            this.labelnome.Name = "labelnome";
+            this.labelnome.Size = new System.Drawing.Size(0, 13);
+            this.labelnome.TabIndex = 26;
+            this.labelnome.Click += new System.EventHandler(this.labelnome_Click);
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.Location = new System.Drawing.Point(244, 377);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(75, 23);
+            this.buttonFechar.TabIndex = 25;
+            this.buttonFechar.Text = "Fechar Aplicação";
+            this.buttonFechar.UseVisualStyleBackColor = true;
+            this.buttonFechar.Click += new System.EventHandler(this.buttonFechar_Click);
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(132, 377);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCadastrar.TabIndex = 24;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(140, 224);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(176, 20);
+            this.textBoxEmail.TabIndex = 23;
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.textBoxEmail_TextChanged);
+            // 
+            // textBoxIdade
+            // 
+            this.textBoxIdade.Location = new System.Drawing.Point(140, 168);
+            this.textBoxIdade.Name = "textBoxIdade";
+            this.textBoxIdade.Size = new System.Drawing.Size(67, 20);
+            this.textBoxIdade.TabIndex = 22;
+            this.textBoxIdade.TextChanged += new System.EventHandler(this.textBoxIdade_TextChanged);
+            // 
+            // textBoxSobrenome
+            // 
+            this.textBoxSobrenome.Location = new System.Drawing.Point(140, 114);
+            this.textBoxSobrenome.Name = "textBoxSobrenome";
+            this.textBoxSobrenome.Size = new System.Drawing.Size(176, 20);
+            this.textBoxSobrenome.TabIndex = 21;
+            this.textBoxSobrenome.TextChanged += new System.EventHandler(this.textBoxSobrenome_TextChanged);
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(140, 58);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(176, 20);
+            this.textBoxNome.TabIndex = 19;
+            this.textBoxNome.TextChanged += new System.EventHandler(this.textBoxNome_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(79, 227);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Email";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Idade";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(78, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Sobrenome";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(506, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(222, 178);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Nome";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelemail);
+            this.Controls.Add(this.labelidade);
+            this.Controls.Add(this.labelsobrenome);
+            this.Controls.Add(this.labelnome);
+            this.Controls.Add(this.buttonFechar);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxIdade);
+            this.Controls.Add(this.textBoxSobrenome);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelemail;
+        private System.Windows.Forms.Label labelidade;
+        private System.Windows.Forms.Label labelsobrenome;
+        private System.Windows.Forms.Label labelnome;
+        private System.Windows.Forms.Button buttonFechar;
+        private System.Windows.Forms.Button buttonCadastrar;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxIdade;
+        private System.Windows.Forms.TextBox textBoxSobrenome;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Formulario.Aula.3/Formulario.Aula.3/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario.Aula._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            string nomeCliente = "";
+            string sobrenomeCliente = "";
+            int idadeCliente;
+            string emailCliente = "";
+            nomeCliente = textBoxNome.Text;
+            sobrenomeCliente = textBoxSobrenome.Text;
+            idadeCliente = int.Parse(textBoxIdade.Text);
+            emailCliente = textBoxEmail.Text;
+
+            //MessageBox.Show("Nome Completo: " + nomeCliente + " " + sobrenomeCliente + "\nIdade: " + idadeCliente + "\nEmail: " + emailCliente);
+
+            labelnome.Text = nomeCliente;
+            labelsobrenome.Text = sobrenomeCliente;
+
+            labelidade.Text = idadeCliente.ToString();
+            labelemail.Text = emailCliente;
+        }
+
+        private void labelidade_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelsobrenome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelnome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelemail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxIdade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSobrenome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
